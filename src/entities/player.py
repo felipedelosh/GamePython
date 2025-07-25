dcd1b871f91a0b29e8c35e782175fa9f03927ef8
felipedelosh <doctorfhernandez@hotmail.com>
2025-07-25 16:36:05 -0500

    refactor: player

--- src/entities/player.py
+++ src/entities/player.py
@@ -2,18 +2,17 @@
 FelipedelosH
 """
 from src.core.assetManager import AssetManager
-from src.core.stateMachine import *
 
 class Player:
     def __init__(self, config) -> None:
+        self.config = config # config\config.json
         self.assetManager = AssetManager.get_instance()
-        self.config = config # Inyect config in Player
         self.sprite = {}
         self.player_look_to = "up"
         self.posX = 500
         self.posY = 200
-        self.max_pos_x = 0
-        self.max_pos_y = 0
+        self.max_pos_x = int(self.config.get("displayW"))
+        self.max_pos_y = int(self.config.get("displayH"))
         self.age = 0
         self.name = "Player"
         self.health = 100
@@ -21,17 +20,15 @@ class Player:
         self.defend = 1
         self.inteligence = 1
         self.stanmina = 10
-        self.velocity = 3
-        # SET PLAYER VALUES
-        self._init_player()
+        self.velocity = int(self.config.get("playerVelocity"))
 
     def player_mouve_up(self):
-        if (self.posY-self.velocity>0):
+        if self.posY-self.velocity > 0:
             self.posY = self.posY - self.velocity
             self.player_look_to = "up"
 
     def player_mouve_down(self):
-        if self.posY+150 < self.max_pos_y: #150 is h of sprite.png
+        if self.posY+150 < self.max_pos_y:
             self.posY = self.posY + self.velocity
             self.player_look_to = "down"
 
@@ -47,10 +44,3 @@ class Player:
 
     def getPlayerSprite(self):
         return self.assetManager.get_sprite("player", self.player_look_to)
-    
-    def _init_player(self):
-        self.max_pos_x = int(self.config.get("displayW"))
-        self.max_pos_y = int(self.config.get("displayH"))
-        self.posX = 200
-        self.posY = 100
-        self.velocity = int(self.config.get("playerVelocity"))

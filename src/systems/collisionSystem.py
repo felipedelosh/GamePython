b0dd261e2df064b8642e16f810d6f44825b975ef
felipedelosh <doctorfhernandez@hotmail.com>
2025-07-28 18:09:32 -0500

    WIP: collitionSystem

--- src/systems/collisionSystem.py
+++ src/systems/collisionSystem.py
@@ -26,26 +26,31 @@ class CollisionSystem(System):
                 sprite_coords = entity.get_component(SpriteCoordsComponent)
                 next_pos = entity.get_component(NextPositionComponent)
 
-                if self._is_walkable(next_pos.x, next_pos.y, sprite_coords):
+                if self._is_walkable(next_pos.x, next_pos.y, size):
                     pos.x, pos.y = next_pos.x, next_pos.y
                     sprite_coords.x = pos.x - (size.w / 2)
                     sprite_coords.y = pos.y - (size.h / 2)
                 else:
                     next_pos.x, next_pos.y = pos.x, pos.y  
 
-    def _is_walkable(self, x, y, sprite_coords):
+    def _is_walkable(self, x, y, size):
         tile_size_w = self.world.w
         tile_size_h = self.world.h
 
         tile_x = int(x // tile_size_w)
         tile_y = int(y // tile_size_h)
 
+
+        if (x - size.w/2) < 0:
+            return False
+        if (y - size.h/2) < 0:
+            return False
+        if (x + size.w/2) > self.player.max_pos_x:
+            return False
+        if (y + size.h/2) > self.player.max_pos_y:
+            return False
+
         try:
-            # Screen
-            if x < 0 or y < 0 or x > self.player.max_pos_x or y > self.player.max_pos_y:
-                return False
-            
-            # Titles
             return self.world.collider[tile_y][tile_x] == 0
         except IndexError:
             return False

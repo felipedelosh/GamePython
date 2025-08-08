4541cb9357998cb0012229f939d7c2cdd69ad8bd
felipedelosh <doctorfhernandez@hotmail.com>
2025-08-08 18:54:47 -0500

    Wip:GamePause

--- src/core/inputHandler.py
+++ src/core/inputHandler.py
@@ -70,9 +70,6 @@ class InputHandler:
                     #     self.gameStateManager.getStateMachine("game").mouvePointer(self.control.key_START)
                         
                     self.controller.current_state.current_pause_menu_option_selected = current_option
-                if keycode == self.control.key_A:
-                    current_option = self.gameStateManager.getStateMachine("pause").pointer
-                    self.controller.current_state.current_pause_menu_option_selected = current_option
                 if keycode == self.control.key_UP:
                     self.gameStateManager.getStateMachine("pause").mouvePointer(self.control.key_UP)
                 if keycode == self.control.key_DOWN:
